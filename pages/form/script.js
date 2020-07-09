02f64e042ff8f9df281b4108fdbd93e50305c545
Maycon <m.vinnicyus@gmail.com>
2020-07-09 17:11:58 -0300

    feat: auto complete address with cep

--- pages/form/script.js
+++ pages/form/script.js
@@ -3,7 +3,33 @@ const selectCity = document.querySelector('#city');
 
 const cepField = document.querySelector('#cep');
 
-window.onload = async () => {
+const neighborhoodField = document.querySelector('#neighborhood');
+const addressField = document.querySelector('#address');
+
+window.onload = async () => setUFs();
+
+selectUf.onchange = async () => {
+  const uf = selectUf.value;
+  updateCities(uf);
+};
+
+cepField.onchange = async () => {
+  const cep = cepField.value;
+  if (cep.length === 8) {
+    const cepInfos = await fetch(
+      `https://viacep.com.br/ws/${cep}/json/`
+    ).then(res => res.json());
+    addressField.value = cepInfos.logradouro;
+    neighborhoodField.value = cepInfos.bairro;
+
+    await updateCities(cepInfos.uf);
+
+    selectUf.value = cepInfos.uf;
+    selectCity.value = cepInfos.localidade;
+  }
+};
+
+async function setUFs() {
   const ufs = await fetch(
     'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
   ).then(res => res.json());
@@ -13,15 +39,16 @@ window.onload = async () => {
   selectUf.innerHTML =
     '<option value="">Selecione seu Estado</option>' +
     ufSorted.map(({ sigla }) => `<option value="${sigla}">${sigla}</option>`);
-};
+}
 
-selectUf.onchange = async () => {
-  const uf = selectUf.value;
+async function updateCities(uf) {
+  selectCity.innerHTML = '<option value="">Selecione sua cidade</option>';
+
   const cities = await fetch(
     `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
   ).then(res => res.json());
 
-  selectCity.innerHTML =
-    '<option value="">Selecione sua cidade</option>' +
-    cities.map(({ nome }) => `<option value="${nome}">${nome}</option>`);
-};
+  selectCity.innerHTML += cities.map(
+    ({ nome }) => `<option value="${nome}">${nome}</option>`
+  );
+}
